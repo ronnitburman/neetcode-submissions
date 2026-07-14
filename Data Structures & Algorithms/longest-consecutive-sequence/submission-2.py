c39b37b57e0ef66434e9587ab0cb95071e2519c2
Ronnit Roy Burman <ronnitburman@gmail.com>
2026-07-14 15:05:47 -0400

    Add: longest-consecutive-sequence - submission-2

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-2.py
@@ -0,0 +1,21 @@
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        #need to use a hashmap to check if there is member to the left of the number present in the set to mark the start of a sequence
+        #then we just need to keep iterating for the loop to see how long the sequence continues if the element is the start of the sequence
+        #the first solution passed, now lets try to optimize for space
+        hash_set = set(nums)
+        max_len = 0
+        def is_seq_starter(num):
+            return not((num-1) in hash_set)
+        def len_of_seq(num):
+            count=0
+            while num in hash_set:
+                count+=1
+                num+=1
+            return count
+
+
+        for num in nums:
+            if is_seq_starter(num):
+                max_len=max(max_len, len_of_seq(num))
+        return max_len
